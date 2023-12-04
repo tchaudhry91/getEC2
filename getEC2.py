e2514a6621bd8fab8431916d838e6624125ba511
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-12-04 15:00:12 +0530

    test

--- getEC2.py
+++ getEC2.py
@@ -2,8 +2,12 @@ import boto3
 
 client = boto3.client('ec2')
 
-conn = boto3.resource('ec2')
-instances = conn.instances.filter()
+import sys
+
+region = sys.argv[1]
+
+conn = boto3.resource('ec2', region=region)
+instances = conn.instances.filter(region=region)
 for instance in instances:
     if instance.state["Name"] == "running":
        print (instance.id, instance.instance_type, region, instance.tags)
