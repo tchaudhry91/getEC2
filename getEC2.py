b78ffef4dee4aa68d3ac79a4021c147cf22097ac
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-12-04 15:12:32 +0530

    sync

--- getEC2.py
+++ getEC2.py
@@ -2,13 +2,26 @@ import boto3
 import sys
 
 region = sys.argv[1]
-
 print("Region:" + region)
 
-client = boto3.client('ec2', region_name=region)
+session=boto3.Session(region_name=region) #boto3 session, aws profile 
 
-conn = boto3.resource('ec2', region_name=region)
-instances = conn.instances.filter()
-for instance in instances:
-    if instance.state["Name"] == "running":
-       print (instance.id, instance.instance_type, region, instance.tags)
+ec2_resource=session.resource(service_name="ec2") # resource object method
+
+'''
+client object method, commented because this script using resource object method
+ec2_client=session.client(service_name="ec2")
+'''
+
+#using Resource Object
+'''
+for each_instances in ec2_resource.instances.all():
+    print(each_instances.id, each_instances.state['Name'])
+'''
+
+for instance in ec2_resource.instances.all():
+     print(
+            "Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(
+         instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state
+         )
+     )
