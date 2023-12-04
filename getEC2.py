55638ede89e7442e9663b3aac08c2ffd7b3fe3fe
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-12-04 15:06:18 +0530

    debug

--- getEC2.py
+++ getEC2.py
@@ -5,10 +5,10 @@ region = sys.argv[1]
 
 print("Region:" + region)
 
-client = boto3.client('ec2', region=region)
+client = boto3.client('ec2', region_name=region)
 
-conn = boto3.resource('ec2', region=region)
-instances = conn.instances.filter(region=region)
+conn = boto3.resource('ec2', region_name=region)
+instances = conn.instances.filter(region_name=region)
 for instance in instances:
     if instance.state["Name"] == "running":
        print (instance.id, instance.instance_type, region, instance.tags)
